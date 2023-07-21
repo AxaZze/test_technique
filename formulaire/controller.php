f6056cd2f66fef8c54363b17d74137f74f8946fa
AxaZze <84575776+AxaZze@users.noreply.github.com>
2023-07-21 03:12:59 +0200

    Ajout commentaire

--- formulaire/controller.php
+++ formulaire/controller.php
@@ -1,17 +1,17 @@
 <?php
-    // Vérifie qu'il provient d'une requête POST
+    // Vérifie que la requête provient d'une méthode POST.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-        // Identifiants MySQL
+        // Définit les identifiants pour la base de données MySQL.
         $host = "localhost";
         $username = "root";
         $password = "";
         $database = "test_technique";
         
-        // Récupère les données du formulaire
+        // Récupère les données soumises par le formulaire.
         $name = $_POST["name"]; 
         $prenom = $_POST["prenom"];
-        $fonction = $_POST["fonction"] ?? NULL;  // Permet à la fonction d'être NULL
+        $fonction = $_POST["fonction"] ?? NULL;  // La fonction peut être NULL si non renseignée.
         $tel = $_POST["tel"];
         $email = $_POST["email"];
         $entreprise = $_POST["entreprise"];
@@ -19,22 +19,25 @@
         $description = $_POST["description"];
 
 
-        // Ouvre une nouvelle connexion au serveur MySQL
+        // Tente d'ouvrir une nouvelle connexion au serveur MySQL.
         $mysqli = new mysqli($host, $username, $password, $database);
         
-        // Affiche toute erreur de connexion
+        // En cas d'erreur de connexion, affiche un message d'erreur.
         if ($mysqli->connect_error) {
-            http_response_code(500);  // Renvoie une erreur HTTP 500
+            http_response_code(500);  // Envoie une réponse HTTP 500 pour signaler l'erreur.
             echo 'Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error;
             exit();
         }
         
+        // Prépare une déclaration SQL pour insérer les données dans la base.
         $statement = $mysqli->prepare("INSERT INTO users (name, prenom, tel, entreprise, fonction, email, demande, description) VALUES(?, ?, ?, ?, ?, ?, ?, ?)"); 
 
+        // Lie les paramètres à la déclaration SQL.
         $statement->bind_param("ssssssss", $name, $prenom, $tel, $entreprise, $fonction, $email, $demande, $description); 
         
+        // Tente d'exécuter la déclaration SQL.
         if($statement->execute()){
-            // Si l'insertion a réussi, envoie un e-mail
+            // Si l'insertion des données a réussi, envoie un e-mail pour notifier de la nouvelle inscription.
             $to = "cyprien.prouvot@gmail.com";
             $subject = "Nouvelle inscription";
             $message = "Un nouvel utilisateur s'est inscrit. Voici les informations fournies :\n\n" . 
@@ -48,16 +51,20 @@
                         "Description : " . $description;
             $headers = "de ". $email;
             
+            // Envoie l'e-mail.
             mail($to, $subject, $message, $headers);
 
-            http_response_code(200);  // Renvoie une réponse HTTP 200
+            // Envoie une réponse HTTP 200 pour signaler le succès de l'opération.
+            http_response_code(200);  
             echo "http 200 ";
         } else {
-            http_response_code(500);  // Renvoie une erreur HTTP 500
+            // En cas d'erreur, envoie une réponse HTTP 500 et affiche le message d'erreur.
+            http_response_code(500);  
             echo $mysqli->error; 
         }
     } else {
-        http_response_code(405);  // Renvoie une erreur HTTP 405 si la méthode n'est pas POST
+        // Si la requête ne provient pas d'une méthode POST, envoie une erreur HTTP 405.
+        http_response_code(405);  
         echo "Méthode non autorisée.";
     }
 ?>
