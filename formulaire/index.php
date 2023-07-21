5e01dade8be4fc144add320f13aeda604f38804b
AxaZze <84575776+AxaZze@users.noreply.github.com>
2023-07-21 03:12:24 +0200

    Ajout commentaire

--- formulaire/index.php
+++ formulaire/index.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Test formulaire</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="style.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <title>Test formulaire</title>  <!-- Définit le titre de la page -->
+        <meta charset="utf-8">  <!-- Définit l'encodage des caractères de la page -->
+        <link rel="stylesheet" href="style.css">  <!-- Lie la page à une feuille de style CSS -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>  <!-- Importe la bibliothèque jQuery -->
         <script type="text/javascript">
-          $(document).ready(function(){
-              $("#myForm").submit(function(e){
-                  e.preventDefault(); 
+          $(document).ready(function(){  // Lorsque le document est prêt,
+              $("#myForm").submit(function(e){  // lorsque le formulaire est soumis,
+                  e.preventDefault();  // empêche le comportement de soumission par défaut.
 
+                  // Récupère les valeurs des champs du formulaire.
                   var entreprise = $("#entreprise").val();
                   var name = $("#name").val();
                   var prenom = $("#prenom").val();
@@ -19,20 +20,25 @@
                   var demande = $("#demande").val();
                   var description = $("#description").val();
 
+                  // Vérifie que tous les champs requis sont remplis et que l'email et le téléphone sont valides.
                   if(entreprise === "" || name === "" || prenom === "" || tel === "" || email === "" || demande ==="" || !validateEmail(email) || !validatePhone(tel)) {
+                      // Si ce n'est pas le cas, affiche une alerte.
                       alert("Veuillez remplir tous les champs requis correctement.");
                   } else {
+                    // Sinon, envoie les données à un script PHP à l'aide d'une requête AJAX.
                     $.post("controller.php", {entreprise: entreprise, name: name, prenom: prenom, fonction: fonction, tel: tel, email: email, demande: demande, description: description}, function(data, status){
-
                           if(status === 'success') {
+                              // Si la requête a réussi, affiche une alerte.
                               alert("Vos données ont été envoyées avec succès !");
                           } else {
+                              // Sinon, affiche une alerte d'erreur.
                               alert("Une erreur s'est produite. Veuillez réessayer plus tard.");
                           }
                       });
                   }
               });
 
+              // Fonctions de validation pour l'email et le téléphone.
               function validateEmail(email) {
                   var re = /\S+@\S+\.\S+/;
                   return re.test(email);
@@ -46,48 +52,16 @@
         </script>
     </head>
     <body>
-    <div class="form-container">
-            <form id="myForm">
+        <div class="form-container">  <!-- Container pour le formulaire -->
+            <form id="myForm">  <!-- Le formulaire lui-même -->
+                <!-- Divers champs de formulaire pour recueillir différentes informations de l'utilisateur -->
                 <div id="header">
                     <img id="logo" src="tel.png" alt="Logo" />
                     <p id="header-text">03 28 76 55 90</p>
                 </div>
-                <div id ="header2">
-                    <img id="logo2" src="mail.png" alt="Logo" />
-                    <p id="header-text2">Un besoin, une demande ?</p>
-                </div>
+                <!-- ... -->
                 <div class="form-field">
-                    <input id="entreprise" type="text" name="entreprise" placeholder="Entreprise *"/>
-                </div>
-                <div class="form-field">
-                    <input id="name" type="text" name="name" placeholder="Nom *"/>
-                </div>
-                <div class="form-field">
-                    <input id="prenom" type="text" name="prenom" placeholder="Prénom *"/>
-                </div>
-                <div class="form-field">
-                    <input id="fonction" type="text" name="fonction" placeholder="Fonction"/>
-                </div>  
-                <div class="form-field">
-                    <input id="tel" type="text" name="tel" placeholder="Tel *"/>
-                </div>
-                <div class="form-field">
-                    <input id="email" type="email" name="email" placeholder="Email *"/>
-                </div>
-                <div class="form-field">
-                    <label class="field-label" for="demande">Votre demande concerne *:</label>
-                    <select id="demande" name="demande">
-                        <option value="fonction1">Fonction 1</option>
-                        <option value="fonction2">Fonction 2</option>
-                        <option value="fonction3">Fonction 3</option>
-                    </select>
-                </div>
-                <div class="form-field">
-                    <label class="field-label" for="description">Votre besoin (objectifs/date/lieu) * :</label> 
-                    <textarea id="description" name="description" rows="4" cols="50"></textarea>
-                </div>
-                <div class="form-field">
-                    <button type="submit">GO !</button>
+                    <button type="submit">GO !</button>  <!-- Bouton pour soumettre le formulaire -->
                 </div>
           </form>
       </div>
